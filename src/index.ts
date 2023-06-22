0d235699a237c892b71a2a48a2e659552543891c
ddanmuji <cgyeo5533@gmail.com>
2023-06-22 21:34:53 +0900

    feat: rooms

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import http from 'http';
 import morgan from 'morgan';
-// import { Server } from 'socket.io';
+import { Server } from 'socket.io';
 
 const app = express();
 
@@ -10,15 +10,19 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
+
 app.use(morgan('dev'));
 
 const server = http.createServer(app);
-// const io = new Server(server, {
-// 	cors: {
-// 		origin: '*',
-// 		methods: ['GET', 'POST']
-// 	}
-// });
+const io = new Server(server);
+
+io.on('connection', socket => {
+	socket.on('join_room', (roomName, done) => {
+		socket.join(roomName);
+		done();
+		socket.to(roomName).emit('welcome');
+	});
+});
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 server.listen(3000, handleListen);
